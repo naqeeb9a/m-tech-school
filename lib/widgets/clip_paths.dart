0e3a50959a737b2da8a461476ca4b3b6178c12c3
InTECHT <88443508+InTECHT@users.noreply.github.com>
2021-11-18 12:00:28 +0500

    Hassan ~ colors touch up

--- lib/widgets/clip_paths.dart
+++ lib/widgets/clip_paths.dart
@@ -11,7 +11,8 @@ class MyClipper extends CustomClipper<Path> {
     path.moveTo(0, sizeCustom == true ? size.height * 0.7 : size.height * 0.2);
     path.lineTo(0, size.height);
     path.lineTo(size.width, size.height);
-    path.lineTo(size.width, sizeCustom == true ? size.height * .2 : size.height * .7);
+    path.lineTo(
+        size.width, sizeCustom == true ? size.height * .2 : size.height * .7);
     return path;
   }
 
