98664a2a943e30071e63d04a499f7db62e496404
InTECHT <88443508+InTECHT@users.noreply.github.com>
2021-11-18 13:01:14 +0500

    Hassan ~ touch up

--- lib/Screens/login_page.dart
+++ lib/Screens/login_page.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:mtech_school_app/Screens/home_screen.dart';
 import 'package:mtech_school_app/utils/config.dart';
+import 'package:mtech_school_app/widgets/dynamic_sizes.dart';
 import 'package:mtech_school_app/widgets/essential_functions.dart';
 import 'package:mtech_school_app/widgets/login_logout.dart';
-import 'package:mtech_school_app/widgets/dynamic_sizes.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginPage extends StatefulWidget {
@@ -22,7 +22,7 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     var userCredentials = [];
     return Scaffold(
-      backgroundColor: myWhite,
+      backgroundColor: myGrey,
       body: (loading == true)
           ? Center(
               child: SizedBox(
@@ -33,44 +33,49 @@ class _LoginPageState extends State<LoginPage> {
           : SingleChildScrollView(
               child: Container(
                 padding: EdgeInsets.symmetric(
-                    horizontal: dynamicWidth(context, 0.1)),
+                  horizontal: dynamicWidth(context, 0.1),
+                ),
                 height: dynamicHeight(context, 1),
                 child: Form(
                   key: _formKey,
                   child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    mainAxisAlignment: MainAxisAlignment.start,
                     children: [
                       SizedBox(
                         height: dynamicHeight(context, 0.4),
                         child: Image.asset("assets/school.png"),
+                      ),
+                      SizedBox(
+                        height: dynamicHeight(context, 0.08),
                       ),
                       inputText("Email", userCredentials, function: (value) {
                         if (value.isEmpty) {
                           return "Enter a Username";
                         }
                       }),
+                      SizedBox(
+                        height: dynamicHeight(context, 0.02),
+                      ),
                       inputText("Password", userCredentials, password: true,
                           function: (value) {
                         if (value.isEmpty) {
                           return "password cannot be empty";
                         }
                       }),
-                      Text(
-                        "Forgot Password?",
-                        style: TextStyle(color: myBlack.withOpacity(0.3)),
-                      ),
                       SizedBox(
-                        height: dynamicHeight(context, 0.01),
+                        height: dynamicHeight(context, 0.1),
                       ),
                       functionalButtons(
                           context,
                           "Login",
                           Icons.arrow_forward,
-                          const Color(0xff6dc9de),
-                          Colors.blue, function: () async {
-                        setState(() {
-                          loading = true;
-                        });
+                          primaryBlue,
+                          primaryBlue.withOpacity(.01), function: () async {
+                        setState(
+                          () {
+                            loading = true;
+                          },
+                        );
                         if (_formKey.currentState!.validate()) {
                           _formKey.currentState!.save();
                           var response = await loginUser(userCredentials);
@@ -100,7 +105,7 @@ class _LoginPageState extends State<LoginPage> {
                         }
                       }),
                       SizedBox(
-                        height: dynamicHeight(context, 0.01),
+                        height: dynamicHeight(context, 0.1),
                       ),
                     ],
                   ),
